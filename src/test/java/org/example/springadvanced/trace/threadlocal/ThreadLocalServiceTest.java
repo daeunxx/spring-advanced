abd4c873da45e68126f4da1038f97fa439f34453
daeun <sss3598@gmail.com>
2024-05-02 21:54:20 +0900

    동시성 문제 해결 -> 쓰레드 로컬 사용한 테스트코드

---
+++ src/test/java/org/example/springadvanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package org.example.springadvanced.trace.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.springadvanced.trace.threadlocal.code.FieldService;
+import org.example.springadvanced.trace.threadlocal.code.ThreadLocalService;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+  private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+  @Test
+  void field() {
+    log.info("main start");
+
+    Runnable userA = () -> threadLocalService.logic("userA");
+    Runnable userB = () -> threadLocalService.logic("userB");
+
+    Thread threadA = new Thread(userA);
+    threadA.setName("thread-A");
+
+    Thread threadB = new Thread(userB);
+    threadB.setName("thread-B");
+
+    threadA.start();
+//    sleep(2000); //동시성 문제 발생X
+    sleep(100); //동시성 문제 발생
+    threadB.start();
+
+    sleep(3000); //메인 쓰레드 종료 대기
+    log.info("main exit");
+  }
+
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
