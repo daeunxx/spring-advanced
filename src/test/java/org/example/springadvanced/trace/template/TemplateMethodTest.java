b634f619a21c49c9927b41a53c02fce5b26de10f
daeun <sss3598@gmail.com>
2024-05-02 23:32:39 +0900

    템플릿 메서드 패턴: 기본 테스트코드

---
+++ src/test/java/org/example/springadvanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,48 @@
+package org.example.springadvanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.springadvanced.trace.template.code.AbstractTemplate;
+import org.example.springadvanced.trace.template.code.SubClassLogic1;
+import org.example.springadvanced.trace.template.code.SubClassLogic2;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+  @Test
+  void templateMethodV0() {
+    logic1();
+    logic2();
+  }
+
+  private void logic1() {
+    long startTime = System.currentTimeMillis();
+
+    //비즈니스 로직 실행
+    log.info("비즈니스 로직1 실행");
+    //비즈니스 로직 종료
+
+    long resultTime = System.currentTimeMillis() - startTime;
+    log.info("resultTime={}", resultTime);
+  }
+
+  private void logic2() {
+    long startTime = System.currentTimeMillis();
+
+    //비즈니스 로직 실행
+    log.info("비즈니스 로직2 실행");
+    //비즈니스 로직 종료
+
+    long resultTime = System.currentTimeMillis() - startTime;
+    log.info("resultTime={}", resultTime);
+  }
+
+  @Test
+  void templateMethodV1() {
+    AbstractTemplate template1 = new SubClassLogic1();
+    template1.execute();
+
+    AbstractTemplate template2 = new SubClassLogic2();
+    template2.execute();
+  }
+}
