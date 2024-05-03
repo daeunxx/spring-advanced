43f723142afa9731e19573f6e9384f294673f9bb
daeun <sss3598@gmail.com>
2024-05-03 10:51:01 +0900

    전략 패턴: 파라미터로 컨텍스트에 전략 전달

---
+++ src/test/java/org/example/springadvanced/trace/strategy/code/ContextV2.java
@@ -0,0 +1,23 @@
+package org.example.springadvanced.trace.strategy.code;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 파라미터로 전략을 보관하는 방식
+ */
+@Slf4j
+@AllArgsConstructor
+public class ContextV2 {
+
+  public void execute(Strategy strategy) {
+    long startTime = System.currentTimeMillis();
+
+    //비즈니스 로직 실행
+    strategy.call(); //위임
+    //비즈니스 로직 종료
+
+    long resultTime = System.currentTimeMillis() - startTime;
+    log.info("resultTime={}", resultTime);
+  }
+}
