40fcd0e7463358f30d5834dffcac333503e3de91
daeun <sss3598@gmail.com>
2024-05-03 21:15:00 +0900

    데코레이터패턴: 기본 테스트코드

---
+++ src/test/java/example/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,27 @@
+package example.proxy.pureproxy.decorator;
+
+import example.proxy.pureproxy.decorator.code.Component;
+import example.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import example.proxy.pureproxy.decorator.code.MessageDecorator;
+import example.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+  @Test
+  void noDecorator() {
+    Component component = new RealComponent();
+    DecoratorPatternClient client = new DecoratorPatternClient(component);
+    client.execute();
+  }
+
+  @Test
+  void decorator1() {
+    Component component = new RealComponent();
+    Component messageDecorator = new MessageDecorator(component);
+    DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+    client.execute();
+  }
+}
---
+++ src/test/java/example/proxy/pureproxy/decorator/code/Component.java
@@ -0,0 +1,6 @@
+package example.proxy.pureproxy.decorator.code;
+
+public interface Component {
+
+  String operation();
+}
