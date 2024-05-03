77413af0dada5036e8f679bf9f06f8ad7215d144
daeun <sss3598@gmail.com>
2024-05-03 16:15:43 +0900

    예제 V1 프로젝트

--- src/main/java/example/proxy/ProxyApplication.java
+++ src/main/java/example/proxy/ProxyApplication.java
@@ -1,8 +1,11 @@
 package example.proxy;
 
+import example.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "example.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/example/proxy/app/v1/OrderServiceV1.java
@@ -0,0 +1,6 @@
+package example.proxy.app.v1;
+
+public interface OrderServiceV1 {
+
+  void orderItem(String itemId);
+}
