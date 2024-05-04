a48eb7e450a9a846f6fc0cfe7865ff4ef30ab40f
daeun <sss3598@gmail.com>
2024-05-04 22:00:49 +0900

    BeanPostProcessor 테스트코드

---
+++ src/test/java/example/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,73 @@
+package example.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanPostProcessorTest {
+
+  @Test
+  void basicConfig() {
+    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+        BeanPostProcessorConfig.class);
+
+    //beanA의 이름으로 B 객체가 빈으로 등록
+    B beanB = applicationContext.getBean("beanA", B.class);
+    beanB.helloB();
+
+    //A는 Bean으로 등록X
+    Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+        () -> applicationContext.getBean(A.class));
+  }
+
+  @Configuration
+  static class BeanPostProcessorConfig {
+
+    @Bean(name = "beanA")
+    public A a() {
+      return new A();
+    }
+    
+    @Bean
+    public AToBPostProcessor postProcessor() {
+      return new AToBPostProcessor();
+    }
+  }
+
+  @Slf4j
+  static class A {
+
+    public void helloA() {
+      log.info("hello A");
+    }
+  }
+
+  @Slf4j
+  static class B {
+
+    public void helloB() {
+      log.info("hello B");
+    }
+  }
+
+  @Slf4j
+  static class AToBPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+        throws BeansException {
+      log.info("beanName={}, bean={}", beanName, bean);
+      
+      if (bean instanceof A) {
+        return new B();
+      }
+      return bean;
+    }
+  }
+}
